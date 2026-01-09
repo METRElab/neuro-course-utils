864ee65525b4dd0bd91a432cc0b01ca64bd85cc6
Matin <mtnusf97@gmail.com>
2026-01-09 08:00:32 -0500

    updating init.py

--- neuro_course_utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .decoding import (
-    plot_poisson_examples,
-    generate_spike_counts,
-    plot_spike_count_distributions,
-    calculate_information_metrics,
-    plot_information_analysis
-)
